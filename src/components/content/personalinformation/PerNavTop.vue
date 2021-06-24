1f51d5bb59848316f93d85828def960d9927ade1
Cherry <1306236708@qq.com>
2021-06-24 11:14:28 +0800

    分枝修改

---
+++ src/components/content/personalinformation/PerNavTop.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="per_nav_top_bar">
+    <img class="per_nav_top_bar_headpic" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PerNavTop",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.per_nav_top_bar {
+  height: 100px;
+  width: 100%;
+  border: 2px solid rgba(175, 175, 175, 0.404);
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+.per_nav_top_bar_headpic {
+  height: 40px;
+  width: 40px;
+  margin: 40px;
+  border-radius: 100%;
+  border: 1px solid rgb(175, 175, 175);
+}
+</style>
